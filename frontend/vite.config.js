41e5288ba18b4b8b4e64cd8e6529c30ef9c3ed2d
Shree-Ram-R <shreeram292004@gmail.com>
2025-04-17 20:08:46 +0530

    Done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'localhost',
+      '1c46-2403-8600-c090-42-0-400-9eb3-1abb.ngrok-free.app',
+      'localhost:5173'
+    ]
+  }
 })
