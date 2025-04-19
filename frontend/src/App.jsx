e5b6bc220536fdfa802d3a8ed6c72a9544527754
Lakshmipriya2005 <lakshmipriya2725@gmail.com>
2025-04-19 11:23:48 +0530

    committed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import "../src/Components/LoginPage/Style.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './Components/Dashboard/Dashboard'
 import ResetPassword from './Components/LoginPage/ResetPassword'
+import './App.css'
+import Home from './Components/Home/Home'
 
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Auth />} />
+      <Route  path="/Home"   element={<Home/>} />
       <Route  path="/Dashboard"   element={<Dashboard/>} />
       <Route  path="/ResetPassword"   element={<ResetPassword/>} />
     </Routes>
