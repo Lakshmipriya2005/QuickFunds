4f997503934afab1c2546ec6d2ced26e0d45bb58
Shree-Ram-R <shreeram292004@gmail.com>
2025-04-19 13:30:19 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Dashboard from './Components/Dashboard/Dashboard'
 import ResetPassword from './Components/LoginPage/ResetPassword'
 import './App.css'
 import Home from './Components/Home/Home'
+import Status from './Components/Status/Status'
+import LoanCalculator from './Components/Calculator/LoanCalculator'
+import { Contact } from 'lucide-react'
+import ApplyLoan from './Components/ApplyLoan/ApplyLoan'
+import About from './Components/About/About'
 
 
 function App() {
@@ -19,6 +24,12 @@ function App() {
       <Route  path="/Home"   element={<Home/>} />
       <Route  path="/Dashboard"   element={<Dashboard/>} />
       <Route  path="/ResetPassword"   element={<ResetPassword/>} />
+      <Route path="/" element={<Auth />} />
+      <Route  path="/Status"   element={<Status/>} />
+      <Route  path="/LoanCalculator"   element={<LoanCalculator/>} />
+      <Route  path="/Contact"   element={<Contact/>} />
+      <Route  path="/ApplyLoan"   element={<ApplyLoan/>} />
+      <Route  path="/About"   element={<About/>} />
     </Routes>
     </BrowserRouter>
      
