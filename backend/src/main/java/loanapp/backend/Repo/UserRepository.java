0fe7aced875775bf6d680166922bf8ae19254545
Lakshmipriya2005 <lakshmipriya2725@gmail.com>
2025-04-29 08:38:34 +0530

    committ

--- backend/src/main/java/loanapp/backend/Repo/UserRepository.java
+++ backend/src/main/java/loanapp/backend/Repo/UserRepository.java
@@ -1,14 +1,13 @@
 package loanapp.backend.Repo;
 
-import java.util.List;
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
+
+
 //import loanapp.backend.Dtos.UserSecureDto;
 import loanapp.backend.Entity.UserEntity;
 
